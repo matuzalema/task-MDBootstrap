73d97e083c84f14e5aec5fefc8387abe3dfabad4
matuzalema <matuzalema@gmail.com>
2019-10-16 03:51:35 +0200

    Add functionality to up and down buttons

--- js/script.js
+++ js/script.js
@@ -41,6 +41,7 @@ function addTask(task) {
     //add HTML part to new task
     const li = document.createElement('li');
     toDoList.appendChild(li);
+    const liList = document.getElementsByTagName("li");
 
     //add input
     const input = document.createElement('input');
@@ -49,14 +50,11 @@ function addTask(task) {
     const inputFields = document.getElementsByClassName("task-input");
     const inputList = inputFields[inputFields.length - 1];
     inputList.setAttribute("value", task);
-
-    
+   
     //edit
     li.insertAdjacentHTML('afterbegin', '<i class="fas fa-user-edit"></i>');
-    
     const edit = document.querySelectorAll('.fa-user-edit');
-
-    edit[edit.length-1].addEventListener('click', function(){
+    edit[edit.length-1].addEventListener('click', () => {
         inputList.removeAttribute("disabled");
         inputList.setAttribute("style", "background-color: #fff; color: #2d3436;");
     })
@@ -66,19 +64,40 @@ function addTask(task) {
     const saveBtn = document.getElementsByClassName("save-btn");
     const saveBtnList = saveBtn[saveBtn.length-1];
 
-    saveBtnList.addEventListener('click', function () {
+    saveBtnList.addEventListener('click', () => {
         inputList.setAttribute("disabled", true);
         inputList.setAttribute("style", "background-color: transparent; color: #fff;");
     })
-
+    
+    //add trash
     li.insertAdjacentHTML('beforeend', '<i class="fas fa-trash"></i>');
-
-    //add eventListener to trash
     const trash = document.querySelectorAll(".fa-trash");
     trash[trash.length - 1].addEventListener('click', function () {
         this.parentNode.remove();
-        taskNumber = 0;
     });
+
+    //add button down
+    li.insertAdjacentHTML('beforeend', '<button class="button btn-move btn-up"> up </button>');
+    const btnUp = document.querySelectorAll('.btn-up');
+    btnUp[btnUp.length - 1].addEventListener('click', function() {
+
+        if (this.parentNode.previousSibling.childNodes.length === 0 ){
+            return;
+            } else {
+                this.parentNode.previousSibling.before(this.parentNode);
+            };
+        });
+
+    //add button up
+    li.insertAdjacentHTML('beforeend', '<button class="button btn-move btn-down"> down </button>');
+    const btnDown = document.querySelectorAll('.btn-down');
+    btnDown[btnDown.length - 1].addEventListener('click', function(){
+        if (this.parentNode.nextSibling.childNodes.length === 0){
+            return;
+        } else {
+            this.parentNode.before(this.parentNode.nextSibling);
+        };
+    })
 }
 
 // add task from input
@@ -86,10 +105,14 @@ let input = document.querySelector(".input-add-task");
 const btnAddTask = document.querySelector(".btn-add-task");
 btnAddTask.addEventListener('click', (e) => {
     e.preventDefault();
-    addTask(input.value);
-    input.value = "";
+    if(input.value===""){
+        return;
+    } else { 
+        addTask(input.value);
+        input.value = "";
+        taskNumber++;
+    }
 });
-
 
 // ============= jQuery ====================
 // let taskNumber = 1;
